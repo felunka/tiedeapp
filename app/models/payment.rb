ebed2c2c359882e105d0f6ab91bb35d98b6040dd
felunka <felixvontiedemann@gmail.com>
2022-02-26 13:02:34 +0100

    Add validations and type to pyment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,24 @@
 class Payment < ApplicationRecord
   belongs_to :registration
   belongs_to :member
+
+  validate :member_xor_registration
+
+  def payment_type
+    if member.present?
+      :member
+    elsif registration.present?
+      :registration
+    else
+      :unknown
+    end
+  end
+
+  private
+
+  def member_xor_registration
+    if !(member.nil? ^ registration.member?)
+      errors.add(:base, I18n.t('model.payment.error.only_member_xor_registration'))
+    end
+  end
 end
