76bb331e64d624c4d4854f2a76a0ecfd139dc947
felunka <felixvontiedemann@gmail.com>
2022-03-02 18:24:01 +0100

    Fix jquery not loaded in production

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
+
 require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
-require('jquery')
+
 // cocoon nested form
 require('@nathanvda/cocoon')
 // bootstrap
