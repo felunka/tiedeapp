c1a32db5cd3795b4c178bb900a4d469a37f07629
felunka <felixvontiedemann@gmail.com>
2022-09-09 21:55:32 +0200

    Add close on second click for datagrid filter popup

--- app/javascript/controllers/datagrid_controller.js
+++ app/javascript/controllers/datagrid_controller.js
@@ -7,67 +7,73 @@ export default class extends Controller {
     let target = event.target.closest('.filter-button');
     let filterInputId = `${target.dataset.gridName}_${target.dataset.columnName}`;
 
-    // Create filter input
-    let header = document.createElement('div');
-    header.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+    if(target.dataset.filterIsOpen) {
+      let popover = bootstrap.Popover.getInstance(target.parentNode.querySelector('.popover'));
+      this.filterClose(popover);
+    } else {
+      // Create filter input
+      let header = document.createElement('div');
+      header.classList.add('d-flex', 'justify-content-between', 'align-items-center');
 
-    let title = document.createElement('span');
-    title.innerText = I18n.datagrid.filters.title;
-    header.appendChild(title);
+      let title = document.createElement('span');
+      title.innerText = I18n.datagrid.filters.title;
+      header.appendChild(title);
 
-    let actionsWrapper = document.createElement('div');
-    actionsWrapper.classList.add('d-flex', 'filter-header-actions');
-    header.appendChild(actionsWrapper);
+      let actionsWrapper = document.createElement('div');
+      actionsWrapper.classList.add('d-flex', 'filter-header-actions');
+      header.appendChild(actionsWrapper);
 
-    let clearButton = document.createElement('i');
-    clearButton.classList.add('fa-solid', 'fa-arrow-rotate-left');
-    clearButton.dataset.action = 'click->datagrid#filterReset';
-    actionsWrapper.appendChild(clearButton);
+      let clearButton = document.createElement('i');
+      clearButton.classList.add('fa-solid', 'fa-arrow-rotate-left');
+      clearButton.dataset.action = 'click->datagrid#filterReset';
+      actionsWrapper.appendChild(clearButton);
 
-    let closeButton = document.createElement('i');
-    closeButton.classList.add('filter-close-button', 'fa-solid', 'fa-times');
-    closeButton.dataset.action = 'click->datagrid#filterClose';
-    actionsWrapper.appendChild(closeButton);
+      let closeButton = document.createElement('i');
+      closeButton.classList.add('filter-close-button', 'fa-solid', 'fa-times');
+      closeButton.dataset.action = 'click->datagrid#filterCloseButtonClick';
+      actionsWrapper.appendChild(closeButton);
 
-    let wrapper = document.createElement('div');
-    wrapper.classList.add('input-group');
+      let wrapper = document.createElement('div');
+      wrapper.classList.add('input-group');
 
-    let input = document.createElement('input');
-    input.classList.add('form-control');
-    input.dataset.action = 'keydown->datagrid#inputKeyDown';
-    input.type = 'text';
-    input.placeholder = target.dataset.label;
-    let filterInput = document.getElementById(filterInputId);
-    input.value = filterInput.value;
-    wrapper.appendChild(input);
+      let input = document.createElement('input');
+      input.classList.add('form-control');
+      input.dataset.action = 'keydown->datagrid#inputKeyDown';
+      input.type = 'text';
+      input.placeholder = target.dataset.label;
+      let filterInput = document.getElementById(filterInputId);
+      input.value = filterInput.value;
+      wrapper.appendChild(input);
 
-    let button = document.createElement('a');
-    button.role = 'button';
-    button.dataset.action = 'click->datagrid#searchButtonClick';
-    button.dataset.filterInputId = filterInputId;
-    button.classList.add('btn', 'btn-outline-primary');
-    wrapper.appendChild(button);
+      let button = document.createElement('a');
+      button.role = 'button';
+      button.dataset.action = 'click->datagrid#searchButtonClick';
+      button.dataset.filterInputId = filterInputId;
+      button.classList.add('btn', 'btn-outline-primary');
+      wrapper.appendChild(button);
 
-    let icon = document.createElement('i');
-    icon.classList.add('fa-solid', 'fa-magnifying-glass');
-    button.appendChild(icon);
+      let icon = document.createElement('i');
+      icon.classList.add('fa-solid', 'fa-magnifying-glass');
+      button.appendChild(icon);
 
-    // Show filter popup
-    let options = {
-      title: header,
-      content: wrapper,
-      placement: 'bottom',
-      html: true,
-      trigger: 'focus',
-      container: target.parentElement
-    };
+      // Show filter popup
+      let options = {
+        title: header,
+        content: wrapper,
+        placement: 'bottom',
+        html: true,
+        trigger: 'focus',
+        container: target.parentElement
+      };
 
-    let popover = new bootstrap.Popover(target, options);
-    popover.show();
+      let popover = new bootstrap.Popover(target, options);
+      popover.show();
+
+      target.dataset.filterIsOpen = true;
+    }
   }
 
   inputKeyDown(event) {
-    console.log(event.keyCode);
     // keyCode == 13 => ENTER Key
     if(event.keyCode == 13) {
       let target = event.target.parentElement.querySelector('a');
@@ -75,7 +81,8 @@ export default class extends Controller {
     }
     // keyCode == 13 => ESC Key
     if(event.keyCode == 27) {
-      this.filterClose(event);
+      let popover = bootstrap.Popover.getInstance(event.target.closest('.popover'));
+      this.filterClose(popover);
     }
   }
 
@@ -101,8 +108,13 @@ export default class extends Controller {
     filterInput.closest('form').submit();
   }
 
-  filterClose(event) {
+  filterCloseButtonClick(event) {
     let popover = bootstrap.Popover.getInstance(event.target.closest('.popover'));
+    this.filterClose(popover);
+  }
+
+  filterClose(popover) {
+    delete popover._element.parentNode.querySelector('.filter-button').dataset.filterIsOpen;
     popover.dispose();
   }
 }
