63948c5dd46295b9f96b779e68ae204083494884
felunka <felixvontiedemann@gmail.com>
2022-09-18 12:05:17 +0200

    Fix grid actions in members grid

--- app/grids/members_grid.rb
+++ app/grids/members_grid.rb
@@ -32,5 +32,27 @@ class MembersGrid < ApplicationGrid
   column(:country)
   column(:member_type) { |asset| I18n.t("simple_form.options.defaults.member_type.#{asset.member_type}") }
 
-  actions([:edit, :destroy])
+  column(:actions, html: true, header: '') do |member|
+    content_tag(:div, class: 'd-flex model-buttons') do
+      if current_user.admin? || current_user.member.id == member.id
+        concat(
+          button_to(edit_member_path(member), class: 'btn btn-primary btn-sm', method: :get) do
+            icon 'pen'
+          end
+        )
+      end
+      if current_user.admin?
+        concat(
+          button_to(member_payments_path(member), class: 'btn btn-primary btn-sm', method: :get) do
+            icon 'money-bill'
+          end
+        )
+        concat(
+          button_to(member_path(member), class: 'btn btn-danger btn-sm', method: :delete, form: { data: { turbo_confirm: t('helpers.submit.confirm') } }) do
+            icon 'trash'
+          end
+        )
+      end
+    end
+  end
 end
