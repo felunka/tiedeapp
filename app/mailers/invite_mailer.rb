25e0d810a4a30b482e5d5bf53df67e75cc8a26bd
Felix von Tiedemann <felix.vontiedemann@oliverwyman.com>
2025-03-17 10:29:19 +0100

    Add reminder mailer

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -17,4 +17,21 @@ class InviteMailer < ApplicationMailer
 
     bootstrap_mail to: @member.email, subject: t('mails.invite.subject', event_title: @event.name)
   end
+
+  def send_reminder(member_event)
+    @member = member_event.member
+    @event = member_event.event
+    @token = member_event.token
+
+    attachments['invite.pdf'] = WickedPdf.new.pdf_from_string(
+      render_to_string('registrations/invitation', layout: 'application'), {encoding: 'UTF-8', header: {
+        content: render_to_string(template: 'layouts/header', layout: 'layouts/application')
+      },
+      margin: {
+        top: 10
+      }}
+    )
+
+    bootstrap_mail to: @member.email, subject: t('mails.reminder.subject', event_title: @event.name)
+  end
 end
