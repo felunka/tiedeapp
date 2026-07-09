9e559a714cf98e4d056ec55fd28c0dc416a79813
felunka <felixvontiedemann@gmail.com>
2026-07-09 21:29:38 +0200

    Create tokens if invited member has none

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,8 +1,5 @@
 module EventsHelper
   def resolve_recipients(event, mail_params, strategy)
-    member_events = MemberEvent.where(event_id: event.id).joins(:member)
-    member_events = member_events.where.not(members: { email: [nil, ''] })
-
     case strategy
     when 'select_members'
       member_ids = Array(mail_params[:recipients]).reject(&:blank?).map(&:to_i)
@@ -10,14 +7,24 @@ module EventsHelper
         redirect_to event_path(event), flash: { danger: t('mails.errors.no_recipients') }
         return nil
       end
-      member_events.where(members: { id: member_ids })
+
+      ensure_member_events(event, member_ids)
+      MemberEvent.where(event_id: event.id, member_id: member_ids)
+                 .joins(:member)
+                 .where.not(members: { email: [nil, ''] })
     when 'select_group'
       member_types = Array(mail_params[:member_groups]).reject(&:blank?)
       if member_types.empty?
         redirect_to event_path(event), flash: { danger: t('mails.errors.no_group_selected') }
         return nil
       end
-      member_events = member_events.where(members: { member_type: member_types })
+
+      group_member_ids = Member.where(member_type: member_types).pluck(:id)
+      ensure_member_events(event, group_member_ids)
+
+      member_events = MemberEvent.where(event_id: event.id, member_id: group_member_ids)
+                                 .joins(:member)
+                                 .where.not(members: { email: [nil, ''] })
       if mail_params[:only_not_registered] == '1'
         registered_member_ids = Registration.joins(:event).where(events: { id: event.id }).pluck(:member_id)
         member_events = member_events.where.not(members: { id: registered_member_ids })
@@ -26,6 +33,14 @@ module EventsHelper
     else
       redirect_to event_path(event), flash: { danger: t('mails.errors.invalid_strategy') }
       nil
+    end
+  end
+
+  private
+
+  def ensure_member_events(event, member_ids)
+    Array(member_ids).uniq.each do |member_id|
+      MemberEvent.find_or_create_by(event_id: event.id, member_id: member_id)
     end
   end
 
